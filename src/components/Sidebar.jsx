7e39aaf041cf008b7f33642276afb28dffa69194
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-10-29 12:06:17 -0300

    style: Fine-tune CSS for improved layout and accessibility
    - Added padding and margin to some elements
    - Converted font size values from pixels to rems for better accessibility
    - Conducted minor adjustments for an overall refined appearance

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,7 +25,7 @@ export default function Sidebar(props) {
     <section className="pane sidebar">
       <div className="sidebar--header">
         <h3>Notes</h3>
-        <button className="new-note" onClick={props.newNote}>+</button>
+        <button className="new-note-button" onClick={props.newNote}>+</button>
       </div>
       {noteElements}
     </section>
